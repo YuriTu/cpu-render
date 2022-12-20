e8d2b15608cb0622f945174dc34aa57eea926e15
YuriTu <tuqiangfe@gmail.com>
2022-12-20 17:02:26 +0800

    feat: update basic bmp gen

--- main.cpp
+++ main.cpp
@@ -140,9 +140,94 @@ Matrix4x4 getProjection(float near, float far, float aspect, float fov) {
     return ortho * orthoToPerspect;
 }
 
-class Scene {
-    std::vector<float> depthBuffer;
-    std::vector<float> frameBuffer;
+
+
+class BmpGenerator {
+    public:
+        int width;
+        int height;
+        const int BYTES_PER_PIXEL = 3;
+        const int FILE_HEADER_SIZE = 14;
+        const int INFO_HEADER_SIZE = 40;
+        BmpGenerator(int _width, int _height){
+            width = _width;
+            height = _height;
+        }
+
+        unsigned char* createBitmapFileHeader(int stride){
+            int fileSize = FILE_HEADER_SIZE + INFO_HEADER_SIZE + (stride  * height);
+            static unsigned char fileHeader[] = {
+                0,0,
+                0,0,0,0,
+                0,0,0,0,
+                0,0,0,0,
+            };
+
+            fileHeader[ 0] = (unsigned char)('B');
+            fileHeader[ 1] = (unsigned char)('M');
+            fileHeader[ 2] = (unsigned char)(fileSize      );
+            fileHeader[ 3] = (unsigned char)(fileSize >>  8);
+            fileHeader[ 4] = (unsigned char)(fileSize >> 16);
+            fileHeader[ 5] = (unsigned char)(fileSize >> 24);
+            fileHeader[10] = (unsigned char)(FILE_HEADER_SIZE + INFO_HEADER_SIZE);
+
+            return fileHeader;
+        }
+
+        unsigned char* createBitmapInfoHeader(){
+            static unsigned char infoHeader[] = {
+                0,0,0,0, /// header size
+                0,0,0,0, /// image width
+                0,0,0,0, /// image height
+                0,0,     /// number of color planes
+                0,0,     /// bits per pixel
+                0,0,0,0, /// compression
+                0,0,0,0, /// image size
+                0,0,0,0, /// horizontal resolution
+                0,0,0,0, /// vertical resolution
+                0,0,0,0, /// colors in color table
+                0,0,0,0, /// important color count
+            };
+
+            infoHeader[ 0] = (unsigned char)(INFO_HEADER_SIZE);
+            infoHeader[ 4] = (unsigned char)(width      );
+            infoHeader[ 5] = (unsigned char)(width >>  8);
+            infoHeader[ 6] = (unsigned char)(width >> 16);
+            infoHeader[ 7] = (unsigned char)(width >> 24);
+            infoHeader[ 8] = (unsigned char)(height      );
+            infoHeader[ 9] = (unsigned char)(height >>  8);
+            infoHeader[10] = (unsigned char)(height >> 16);
+            infoHeader[11] = (unsigned char)(height >> 24);
+            infoHeader[12] = (unsigned char)(1);
+            infoHeader[14] = (unsigned char)(BYTES_PER_PIXEL*8);
+
+            return infoHeader;
+        }
+
+        void export(std::vector<std::vector<std::vector<float>>> *buffer, char* fileName) {
+            int widthInBytes = width * BYTES_PER_PIXEL;
+
+            unsigned char padding[3] = {0,0,0};
+            int paddingSize = (4 - (widthInBytes) % 4) % 4;
+
+            int stride = (widthInBytes) + paddingSize;
+
+            FILE* imageFile = fopen(fileName, "wb");
+
+            unsigned char* fileHeader = createBitmapFileHeader(stride);
+            fwrite(fileHeader, 1, FILE_HEADER_SIZE, imageFile);
+
+            unsigned char* infoHeader = createBitmapInfoHeader();
+            fwrite(infoHeader, 1, INFO_HEADER_SIZE, imageFile);
+
+            
+            for (int i = 0; i < height; i++) {
+                fwrite(buffer + (i*widthInBytes), BYTES_PER_PIXEL, width, imageFile);
+                fwrite(padding, 1, paddingSize, imageFile);
+            }
+
+            fclose(imageFile);
+        }
 };
 
 bool inTrangle(float x, float y, std::vector<Vector4> points) {
@@ -174,7 +259,8 @@ std::vector<Vector4> getPoints(){
     return ret;
 }
 
-void rasterization (std::vector<float> &frameBuffer, std::vector<Vector4> points) {
+void rasterization (std::vector<std::vector<std::vector<float>>> &frameBuffer, std::vector<Vector4> points) {
+    std::vector<float> tempColor = {255.0,255.0,255.0};
     float x_min = 0;
     float x_max = 0;
     float y_min = 0;
@@ -196,6 +282,7 @@ void rasterization (std::vector<float> &frameBuffer, std::vector<Vector4> points
             if (i < x_max && i > x_min && j < y_max && j > y_min) {
                 if (inTrangle(i,j, points)) {
                     flag = true;
+                    frameBuffer[i][j] = tempColor;
                 }
             }
 
@@ -227,8 +314,10 @@ void rasterization (std::vector<float> &frameBuffer, std::vector<Vector4> points
 
 int main(int argc, char const *argv[])
 {
+    std::vector<std::vector<std::vector<float>>> frameBuffer;
+    // float *** frameBuffer;
     // vector 1 2 3
-    
+
     std::vector<Vector4> points = getPoints();
     
     // scene
@@ -266,12 +355,16 @@ int main(int argc, char const *argv[])
 
     // rasterasing
     // 根据aabb判断哪些像素点需要被着色
+    rasterization(frameBuffer, points);
 
     // 根据三角形重力坐标计算颜色的插值
 
     // 根据depth buffer 判断是否需要把color buffer更新
 
     // 输出结果
+
+    BmpGenerator bmp = BmpGenerator(WIDTH, HEIGHT);
+    bmp.export(frameBuffer,"test.bmp");
 
     
 
