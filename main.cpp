f09a08bcf9a2bebca8fef934fece0ed761bb6925
YuriTu <tuqiang_ru@163.com>
2022-12-22 21:14:40 +0800

    feat: finish rasterization

--- main.cpp
+++ main.cpp
@@ -116,8 +116,8 @@ Matrix4x4 getProjection(float near, float far, float aspect, float fov) {
     ortho = orthoScale * orthoTranslate;
 
     float o2p[4][4] = {
-        {near, 0.0,0.0,-(l+r) /2},
-        {0.0, near,0.0,-(t+b) /2},
+        {near, 0.0,0.0,0.0},
+        {0.0, near,0.0,0.0},
         {0.0, 0.0,near + far,-(near * far)},
         {0.0, 0.0,near,0.0},
     };
@@ -132,28 +132,21 @@ void exportImg(std::vector<Vector4f> frameBuffer){
     int w = WIDTH;
     int h = HEIGHT;
     (void)fprintf(fp, "P6\n%d %d\n255\n", w, h);
-    std::cout << " export framebuffer size" << frameBuffer.size() << std::endl;
     for (int i = 0; i < frameBuffer.size(); i++) {
         static unsigned char color[3];
         
         Vector4f pixel = frameBuffer[i];
-        // std::cout << " export index" << i << std::endl;
         color[0] = (char)(255 * clamp(1.0,0., pixel.x));
         color[1] = (char)(255 * clamp(1.0,0., pixel.y));
         color[2] = (char)(255 * clamp(1.0,0., pixel.z));
-        if (pixel.x > 0 || pixel.y > 0) {
-            std::cout << " frame: x:" << pixel.x << "y:" << pixel.y << 255.0 * clamp(1.0,0., pixel.x) << std::endl;
-            std::cout << " frame: index:" << i << std::endl;
-            std::cout << " color: x:" << color[0] << "y:" << color[1] << std::endl;
-        }
-
-        if (i > 4000) {
-            std::cout << "602  frame: index:" << i << std::endl;
-        }
+        // if (pixel.x > 0 || pixel.y > 0) {
+        //     std::cout << " frame: x:" << pixel.x << "y:" << pixel.y << 255.0 * clamp(1.0,0., pixel.x) << std::endl;
+        //     std::cout << " frame: index:" << i << std::endl;
+        //     std::cout << " color: x:" << color[0] << "y:" << color[1] << std::endl;
+        // }
         
         fwrite(color, 1, 3, fp);
     }
-    std::cout << " ????"<< std::endl;
 
     fclose(fp);
 }
@@ -164,9 +157,15 @@ bool inTrangle(float x, float y, std::vector<Vector4f> points) {
     Vector4f l1 = points[1] - points[2];
     Vector4f l2 = points[2] - points[0];
 
-    float r0 = (l0.cross(l1)).z;
-    float r1 = (l1.cross(l2)).z;
-    float r2 = (l2.cross(l0)).z;
+    Vector4f p = Vector4f(x,y,0.0);
+
+    Vector4f l00 = p - points[1];
+    Vector4f l11 = p - points[2];
+    Vector4f l22 = p - points[0];
+
+    float r0 = (l0.cross(l00)).z;
+    float r1 = (l1.cross(l11)).z;
+    float r2 = (l2.cross(l22)).z;
 
     ret = (r0 >=0 && r1 >=0 && r2 >=0 )|| (r0 <0 && r1 <0 && r2 <0);
     return ret;
@@ -194,7 +193,6 @@ void rasterization (std::vector<Vector4f> &frameBuffer, std::vector<Vector4f> po
         y_min = std::min(y_min, point.y);
         y_max = std::max(y_max, point.y);
     }
-
     for (int i = 0; i < WIDTH; i++) {
         for (int j = 0; j < HEIGHT; j++){
             bool flag = false;
@@ -202,13 +200,8 @@ void rasterization (std::vector<Vector4f> &frameBuffer, std::vector<Vector4f> po
             if (i < x_max && i > x_min && j < y_max && j > y_min) {
                 if (inTrangle(i,j, points)) {
                     flag = true;
-                    int index = j * HEIGHT + i;
+                    int index = (HEIGHT - j) * WIDTH + i;
                     (frameBuffer[index]) = defaultColor;
-                    // memcpy(&(frameBuffer[index]), &defaultColor, sizeof(defaultColor));
-                    
-                        // printf("in tr x:%d, y:%d ind:%d  r:%d defR:%d\n",i,j,index,frameBuffer[index].x,defaultColor.x);    
-                    // std::cout << "in tr x:" << i << "y:" << j << "index:" << index << "x:" << frameBuffer[index].x << std::endl;
-                    
                 }
             }
 
@@ -258,8 +251,8 @@ int main(int argc, char const *argv[])
     // v matrix
     // set look at(0,0,0) \ up direct (0,1,0) \ position (0,0,-1)
     float viewMatrix[4][4] = {
-        1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
+        1.0, 0.0, 0.0, -(0.0),
+        0.0, 1.0, 0.0, -(0.0),
         0.0, 0.0, 1.0, -(-1.0),
         0.0, 0.0, 0.0, 1.0
     };
